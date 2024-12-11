258f173aaae21aef87c386db0f702d13a13c891e
AchavinP <guesttoblame2506@gmail.com>
2024-12-11 11:16:32 +0700

    Add files via upload

---
+++ Labsheet03/src/Lab303.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Lab303 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner("System.in");
+		System.out.print("Input a sentence : ");
+		String sentence = scan.nextLine();
+		while (sentence.endsWith(".")) {
+			if () {
+				
+			}
+		}
+	}
+
+}
