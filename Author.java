415c6a618010548e3e053f7e2c2eabdbfaa59737
AchavinP <guesttoblame2506@gmail.com>
2025-02-19 10:44:23 +0700

    Labsheet10 Example

--- Author.java
+++ Author.java
@@ -1,33 +1,8 @@
 
-public class Author {
-	private String name;
-	private String email;
-	
-	
-	Author() {
-		this(null,null);
-	}
-	
-	Author(String name,String email){
-		this.name = name;
-		this.email = email;
-	}
-	
-	public String getName() {
-		return this.name;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public String getEmail() {
-		return this.email;
-	}
-	
-	public String toString() {
-		return getName()+ " ("+getEmail()+")";
-	}
-	
+public interface Author {
+	public String getLastName();
+	public String getFirstName();
+	public boolean checkEmail();
 	
 	
 }
